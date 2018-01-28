4872fb0017ec02d416380c43539a1ecf0a9446fe
Lars Schotte <gustik@gustik.eu>
2018-01-28 03:15:29 +0100

    added some builders

--- src/work/schotte/Publisher.java
+++ src/work/schotte/Publisher.java
@@ -3,7 +3,10 @@ package work.schotte;
 public class Publisher {
 
   public static void main(String[] args) {
-    System.out.println("ok");
+    // Build front page object with indexes.
+    // Build article pages in parallel thread.
+    // We only need to know what our article pages are, when we start building front page.
+    // Article pages we can build in parallel instantly after discovering articles in a directory. (for that we need directory traversing)
 
   }
 
